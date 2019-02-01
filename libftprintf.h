bcb13df36d4e28b7981bcc1932aa2cf06ca03963
Camille Julien <cajulien@MacBook-Pro-de-Camille.local>
2019-02-01 17:35:56 +0100

    .h

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: cajulien <cajulien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 15:36:29 by pommedepin        #+#    #+#             */
-/*   Updated: 2019/02/01 16:32:00 by cajulien         ###   ########.fr       */
+/*   Updated: 2019/02/01 17:21:33 by cajulien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@
 # include <stdlib.h>
 # include <stdarg.h> // permet l'accès à va_list, va_arg, va_end & va_start
 
-# define FLAGSET "cspdiouxXfegbrk"
+# define SPECSET "cspdiouxXfegbrk"
+# define OPTSET ""
+# define WIDTHSET "0123456789*"
+# define PRECSET ".0123456789*"
+# define LENGTHSET "hlL"
 
 typedef	struct				s_flags
 {
@@ -40,12 +44,12 @@ typedef struct 				s_data
 typedef struct				s_plendid
 {
 	int						narg;
-	t_all					*data;
+	t_data					*data;
 }							t_plendid;
 
 void    					parse(char *str, t_data **data, t_plendid *s);
 t_data      				*create_elem(t_data *data);
-void						show_data(t_data **data);
+void						show_data(t_data **data, t_plendid *s);
 void						free_data(t_data **data, t_plendid *s);
-char						is_spec(char c);
+char						is_spec(char c, char *charset);
 #endif
