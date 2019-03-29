c11ff1875f3b3a3810534ba98a836d2023845bca
Clemzerdu75 <cfauvelll@42.student.fr>
2019-03-29 17:31:50 +0100

    petite correc de l'option 0

--- src/conversion.c
+++ src/conversion.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   conversion.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pommedepin <pommedepin@student.42.fr>      +#+  +:+       +#+        */
+/*   By: cfauvell <cfauvell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/07 23:01:56 by cfauvell          #+#    #+#             */
-/*   Updated: 2019/03/23 18:15:06 by pommedepin       ###   ########.fr       */
+/*   Updated: 2019/03/29 17:31:21 by cfauvell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*handle_field(t_flag flag)
 	if (flag.field > (int)ft_strlen(flag.to_print) && ft_chrstring(flag.option, "-") == 1)
 		flag.to_print = space_fill_r(flag.to_print, flag.field);
 	if (flag.field > (int)ft_strlen(flag.to_print) && ft_chrstring(flag.option, "-") != 1
-		&& ft_chrstring(flag.option, "0") == 1 && flag.precision == 0)
+		&& ft_chrstring(flag.option, "0") == 1 && flag.precision <= 0)
 		flag.to_print = zero_fill_l(flag.to_print, flag.field);
 	if (flag.field > (int)ft_strlen(flag.to_print) && ft_chrstring(flag.option, "-") != 1
 		&& ft_chrstring(flag.option, "0") != 1)
