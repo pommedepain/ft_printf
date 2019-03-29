02d3412c4e66d547a4191a86e97b3bf6d3ba8796
Philippine SENTILHES <psentilh@e2r2p19.42.fr>
2019-03-29 17:58:45 +0100

    annulation du commit de clem

--- src/pf_arg_o.c
+++ src/pf_arg_o.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pf_arg_o.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cfauvell <cfauvell@student.42.fr>          +#+  +:+       +#+        */
+/*   By: psentilh <psentilh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 14:50:12 by benjamintle       #+#    #+#             */
-/*   Updated: 2019/03/29 17:35:16 by cfauvell         ###   ########.fr       */
+/*   Updated: 2019/03/29 17:57:48 by psentilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,30 @@ char	*ft_flag_o(va_list list, t_flag flag)
 	{
 		if (ft_strcmp(flag.modif, "ll") == 0)
 			flag.to_print = ft_ulltoa_base(va_arg(list, unsigned long long), 8, _x_);
-		if (ft_strcmp(flag.modif, "h") == 0)
-			flag.to_print = ft_ulltoa_base(va_arg(list, unsigned int), 8, _x_);
-		if (ft_strcmp(flag.modif, "l") == 0)
+		if (ft_strcmps(flag.modif, "h") == 0)
+			flag.to_print = ft_ulltoa_base((unsigned short)va_arg(list, unsigned int), 8, _x_);
+		if (ft_strcmps(flag.modif, "z") == 0)
+			flag.to_print = ft_ulltoa_base(va_arg(list, ssize_t), 8, _x_);
+		if (ft_strcmps(flag.modif, "l") == 0)
 			flag.to_print = ft_ulltoa_base(va_arg(list, unsigned long), 8, _x_);
-		if (ft_strcmp(flag.modif, "hh") == 0)
-			flag.to_print = ft_ulltoa_base(va_arg(list, unsigned int), 8, _x_);
+		if (ft_strcmps(flag.modif, "hh") == 0)
+			flag.to_print = ft_ulltoa_base((unsigned char)va_arg(list, unsigned int), 8, _x_);
+		if (ft_strcmps(flag.modif, "j") == 0)
+			flag.to_print = ft_ulltoa_base(va_arg(list, intmax_t), 8, _x_);
 	}
 	else
-		flag.to_print = ft_ulltoa_base(va_arg(list, unsigned long long), 8, _x_);
+		flag.to_print = ft_ulltoa_base(va_arg(list, unsigned int), 8, _x_);
 	if (flag.precision < 0 && ft_chrstring(flag.to_print, "0"))
 		flag.to_print = ft_strdups("");
+	if (flag.precision >= 0)
+		flag.to_print = zero_fill(flag.to_print, flag.precision);
 	if (ft_chrstring(flag.option, "#") == 1)
 	{
 		flag.field -= 1;
-		if(flag.precision > 0)
-		flag.precision -= 1;
+		flag.to_print = handle_field(flag);
 	}
-	if (flag.precision >= 0)
-		flag.to_print = zero_fill(flag.to_print, flag.precision);
-	flag.to_print = handle_field(flag);
+	else
+		flag.to_print = handle_field(flag);
 	if ((ft_chrstring(flag.option, "#") == 1))
 	{
 		if (flag.precision == -1)
