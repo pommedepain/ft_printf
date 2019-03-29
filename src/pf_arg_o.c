26699f20e79c50db01a98e9fd7eaa4f21e0a8114
Clemzerdu75 <cfauvelll@42.student.fr>
2019-03-29 14:33:00 +0100

    Le test n5 du test 92 du moulitest a ete regle gestion de 0 pour o

--- src/pf_arg_o.c
+++ src/pf_arg_o.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pf_arg_o.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cajulien <cajulien@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cfauvell <cfauvell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 14:50:12 by benjamintle       #+#    #+#             */
-/*   Updated: 2019/03/28 12:46:01 by cajulien         ###   ########.fr       */
+/*   Updated: 2019/03/29 14:30:47 by cfauvell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,6 @@
 
 char	*ft_flag_o(va_list list, t_flag flag)
 {
-	/*unsigned long	res;
-	int				arg;
-
-	res = 0;
-	arg = va_arg(list, int);
-	if (arg < 0)
-		res = UINT_MAX + arg;
-	else
-		res = arg;*/
 	if (flag.modif)
 	{
 		if (ft_strcmp(flag.modif, "ll") == 0)
@@ -36,9 +27,9 @@ char	*ft_flag_o(va_list list, t_flag flag)
 	}
 	else
 		flag.to_print = ft_ulltoa_base(va_arg(list, unsigned long long), 8, _x_);
-	if (flag.precision < 0)
+	if (flag.precision < 0 && ft_chrstring(flag.to_print, "0"))
 		flag.to_print = ft_strdups("");
-	else if (flag.precision >= 0)
+	if (flag.precision >= 0)
 		flag.to_print = zero_fill(flag.to_print, flag.precision);
 	if (ft_chrstring(flag.option, "#") == 1)
 	{
@@ -50,7 +41,7 @@ char	*ft_flag_o(va_list list, t_flag flag)
 	if ((ft_chrstring(flag.option, "#") == 1))
 	{
 		if (flag.precision == -1)
-			flag.to_print = ft_strdups("0");
+			flag.to_print = ft_strjoin("0", flag.to_print);
 		else if (ft_char_only(flag.to_print, '0', ' ') == 1)
 			flag.to_print = ft_strdups(flag.to_print);
 		else
