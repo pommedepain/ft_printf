1951cecf769cdb93539796ff9edf8687a597e045
Camille JULIEN <cajulien@e2r2p20.42.fr>
2019-03-29 16:48:33 +0100

    ulltoa / lltoa opti

--- src/ft_lltoa_base.c
+++ src/ft_lltoa_base.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lltoa_base.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pommedepin <pommedepin@student.42.fr>      +#+  +:+       +#+        */
+/*   By: cajulien <cajulien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 01:13:00 by cajulien          #+#    #+#             */
-/*   Updated: 2019/03/12 10:41:45 by pommedepin       ###   ########.fr       */
+/*   Updated: 2019/03/29 16:47:41 by cajulien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_lltoa_base(long long value, int base_size, char *base)
+char	*ft_lltoa_base(long long value, int b_size, char *base)
 {
 	int						i;
 	unsigned long long		nbr;
@@ -23,17 +23,20 @@ char	*ft_lltoa_base(long long value, int base_size, char *base)
 		nbr = value * -1;
 	else
 		nbr = value;
-	size = ft_numlen(nbr, base_size);
-	(base_size == 10 && value < 0) ? size++ : 0;
+	size = ft_numlen(nbr, b_size);
+	(b_size == 10 && value < 0) ? size++ : 0;
 	if (!(to_print = (char *)malloc(sizeof(char) * (size + 1))))
 		return (NULL);
 	i = size;
 	while (i--)
 	{
-		to_print[i] = base[nbr % base_size];
-		nbr /= base_size;
+		to_print[i] = base[nbr % b_size];
+		(b_size != 2 && b_size != 8 && b_size != 16) ? nbr /= b_size : 0;
+		b_size == 2 ? nbr >>= 1 : 0;
+		b_size == 8 ? nbr >>= 3 : 0;
+		b_size == 16 ? nbr >>= 4 : 0;
 	}
-	(base_size == 10 && value < 0) ? to_print[0] = '-' : 0;
+	(b_size == 10 && value < 0) ? to_print[0] = '-' : 0;
 	to_print[size] = '\0';
 	return (to_print);
 }
--- src/ft_ulltoa_base.c
+++ src/ft_ulltoa_base.c
@@ -3,29 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ulltoa_base.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pommedepin <pommedepin@student.42.fr>      +#+  +:+       +#+        */
+/*   By: cajulien <cajulien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 01:14:12 by cajulien          #+#    #+#             */
-/*   Updated: 2019/03/23 11:14:33 by pommedepin       ###   ########.fr       */
+/*   Updated: 2019/03/29 16:46:08 by cajulien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_ulltoa_base(unsigned long long value, int base_size, char *base)
+char	*ft_ulltoa_base(unsigned long long n, int b_size, char *base)
 {
 	int						i;
 	int						size;
 	char					*to_print;
 
-	size = ft_numlen(value, base_size);
+	size = ft_numlen(n, b_size);
 	if (!(to_print = (char *)malloc(sizeof(char) * (size + 1))))
 		return (NULL);
 	i = size;
 	while (i--)
 	{
-		to_print[i] = base[value % base_size];
-		value /= base_size;
+		to_print[i] = base[n % b_size];
+		(b_size != 2 && b_size != 8 && b_size != 16) ? n /= b_size : 0;
+		b_size == 2 ? n >>= 1 : 0;
+		b_size == 8 ? n >>= 3 : 0;
+		b_size == 16 ? n >>= 4 : 0;
 	}
 	to_print[size] = '\0';
 	return (to_print);
