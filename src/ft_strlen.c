4a182f4d9ab4a7551f7aea5b356746050bc50255
Camille JULIEN <cajulien@e2r2p20.42.fr>
2019-03-29 16:32:48 +0100

    ft_strlen.c

--- src/ft_strlen.c
+++ src/ft_strlen.c
@@ -6,37 +6,48 @@
 /*   By: cajulien <cajulien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 12:30:50 by cajulien          #+#    #+#             */
-/*   Updated: 2019/03/29 15:20:39 by cajulien         ###   ########.fr       */
+/*   Updated: 2019/03/29 16:32:33 by cajulien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+static size_t	ft_byte_mask(unsigned long long c)
+{
+	if (!(c & 0xFF))
+		return (1);
+	if (!(c & 0xFF00))
+		return (2);
+	if (!(c & 0xFF0000))
+		return (3);
+	if (!(c & 0xFF000000))
+		return (4);
+	if (!(c & 0xFF00000000))
+		return (5);
+	if (!(c & 0xFF0000000000))
+		return (6);
+	if (!(c & 0xFF000000000000))
+		return (7);
+	if (!(c & 0xFF00000000000000))
+		return (8);
+	else
+		return (0);
+}
+
+size_t			ft_strlen(const char *s)
 {
 	size_t				len;
-	unsigned long long	c;
+	unsigned long long	*c;
+	size_t				ret;
 
 	len = 0;
-	while ((c = *(unsigned long long *)s) || 1)
+	c = (unsigned long long *)s;
+	while (1)
 	{
-		if (!(c & 0xFF))
-			return (len);
-		if (!(c & 0xFF00))
-			return (len + 1);
-		if (!(c & 0xFF0000))
-			return (len + 2);
-		if (!(c & 0xFF000000))
-			return (len + 3);
-		if (!(c & 0xFF00000000))
-			return (len + 4);
-		if (!(c & 0xFF0000000000))
-			return (len + 5);
-		if (!(c & 0xFF000000000000))
-			return (len + 6);
-		if (!(c & 0xFF00000000000000))
-			return (len + 7);
-		s += 8;
+		ret = ft_byte_mask(*c);
+		if (ret > 0)
+			return (len + ret - 1);
+		c++;
 		len += 8;
 	}
 }
