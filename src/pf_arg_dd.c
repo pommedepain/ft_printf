1a6bde04ea59fedbadde5b44bff52eaef408d57d
Philippine SENTILHES <psentilh@e2r2p19.42.fr>
2019-03-29 17:34:04 +0100

    test 08 corrige

--- src/pf_arg_dd.c
+++ src/pf_arg_dd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pf_arg_dd.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: benjamintle <benjamintle@student.42.fr>    +#+  +:+       +#+        */
+/*   By: psentilh <psentilh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 14:50:19 by benjamintle       #+#    #+#             */
-/*   Updated: 2019/03/28 17:27:46 by benjamintle      ###   ########.fr       */
+/*   Updated: 2019/03/29 17:31:11 by psentilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_flag_dd(va_list list, t_flag flag)
 	}
 	else
 	{
-		flag.to_print = ft_ulltoa_base(va_arg(list, long long), 10, _x_);
+		flag.to_print = ft_lltoa_base(va_arg(list, long long), 10, _x_);
 		return (flag.to_print);
 	}
 	if (ft_chrstring(flag.option, "+ ") == 1)
