dbd5088152546e8ba47b45eaa36ac30705dc1b08
Benjamin TOLLIE <btollie@e2r2p20.42.fr>
2019-04-03 16:17:28 +0200

    norme arg_manager ok

--- src/pf_arg_manager.c
+++ src/pf_arg_manager.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pf_arg_manager.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psentilh <psentilh@student.42.fr>          +#+  +:+       +#+        */
+/*   By: btollie <btollie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/20 20:20:18 by cfauvell          #+#    #+#             */
-/*   Updated: 2019/04/03 13:35:06 by psentilh         ###   ########.fr       */
+/*   Updated: 2019/04/03 16:16:33 by btollie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,31 @@
 ** for the conversion (with the call of fill_flag)
 */
 
-int		ft_parsing(const char *format, int *i, va_list list)
+static	char	*fill_flag_toprint(va_list list, t_flag flag)
+{
+	(flag.flag == 'c' || flag.flag == 'C') ?
+	flag.to_print = ft_flag_c(list, flag) : 0;
+	(flag.flag == 's' || flag.flag == 'S') ?
+	flag.to_print = ft_flag_s(list, flag) : 0;
+	flag.flag == 'p' ? flag.to_print = ft_flag_p(list, flag) : 0;
+	(flag.flag == 'd' || flag.flag == 'i') ?
+	flag.to_print = ft_flag_d(list, flag) : 0;
+	flag.flag == 'D' ? flag.to_print = ft_flag_dd(list, flag) : 0;
+	flag.flag == 'o' ? flag.to_print = ft_flag_o(list, flag) : 0;
+	flag.flag == 'O' ? flag.to_print = ft_flag_oo(list, flag) : 0;
+	flag.flag == 'u' ? flag.to_print = ft_flag_u(list, flag) : 0;
+	flag.flag == 'U' ? flag.to_print = ft_flag_uu(list, flag) : 0;
+	flag.flag == 'x' ? flag.to_print = ft_flag_x(list, flag) : 0;
+	flag.flag == 'b' ? flag.to_print = ft_flag_b(list, flag) : 0;
+	flag.flag == 'X' ? flag.to_print = ft_flag_xx(list, flag) : 0;
+	(flag.flag == 'f' || flag.flag == 'F') ?
+	flag.to_print = ft_flag_f(list, flag) : 0;
+	flag.flag == '%' ? flag.to_print = ft_flag_per(flag) : 0;
+	flag.flag == 'Z' ? flag.to_print = ft_flag_z(flag) : 0;
+	return (flag.to_print);
+}
+
+int				ft_parsing(const char *format, int *i, va_list list)
 {
 	t_flag	flag;
 	int		res;
@@ -31,50 +55,31 @@ int		ft_parsing(const char *format, int *i, va_list list)
 	if (flag.parsing == NULL)
 		return (0);
 	flag = fill_flag(flag, list);
-	(flag.flag == 'c' || flag.flag == 'C') ? flag.to_print = ft_flag_c(list, flag) : 0;
-	(flag.flag == 's' || flag.flag == 'S')  ? flag.to_print = ft_flag_s(list, flag) : 0;
-	flag.flag == 'p' ? flag.to_print = ft_flag_p(list, flag) : 0;
-	(flag.flag == 'd' || flag.flag == 'i') ? flag.to_print = ft_flag_d(list, flag) : 0;
-	flag.flag == 'D' ? flag.to_print = ft_flag_dd(list, flag) : 0;
-	flag.flag == 'o' ? flag.to_print = ft_flag_o(list, flag) : 0;
-	flag.flag == 'O' ? flag.to_print = ft_flag_oo(list, flag) : 0;
-	flag.flag == 'u' ? flag.to_print = ft_flag_u(list, flag) : 0;
-	flag.flag == 'U' ? flag.to_print = ft_flag_uu(list, flag) : 0;
-	flag.flag == 'x' ? flag.to_print = ft_flag_x(list, flag) : 0;
-	flag.flag == 'b' ? flag.to_print = ft_flag_b(list, flag) : 0;
-	flag.flag == 'X' ? flag.to_print = ft_flag_xx(list, flag) : 0;
-	(flag.flag == 'f' || flag.flag == 'F') ? flag.to_print = ft_flag_f(list, flag) : 0;
-	flag.flag == '%' ? flag.to_print = ft_flag_per(flag) : 0;
-	flag.flag == 'Z' ? flag.to_print = ft_flag_z(flag) : 0;
+	flag.to_print = fill_flag_toprint(list, flag);
 	*i += ft_strlen(flag.parsing);
-	/*ft_putchar('\n');
-	print_struct(flag);
-	ft_putchar('\n');*/
-	if (flag.flag == 'c' && ((ft_strlen(flag.to_print) == 0) || (flag.field != 0 && (ft_char_only(flag.to_print, ' ', '\0') == 1))))
+	if (flag.flag == 'c' && ((ft_strlen(flag.to_print) == 0)
+	|| (flag.field != 0 && (ft_char_only(flag.to_print, ' ', '\0') == 1))))
 	{
 		ft_putstrbk0(flag.to_print, (ft_strlen(flag.to_print) + 1));
 		res = ft_strlen(flag.to_print) + 1;
-		free(flag.to_print);
 	}
 	else
 	{
 		ft_putstr(flag.to_print);
 		res = ft_strlen(flag.to_print);
-		free(flag.to_print);
 	}
-	free(flag.parsing);
-	free(flag.option);
-	free(flag.modif);
+	pf_free_struct(&flag);
 	return (res);
 }
 
-void    ft_putstrbk0(char *str, int len)
+void			ft_putstrbk0(char *str, int len)
 {
-    int i = 0;
+	int i;
 
-    while (i < len)
-    {
-        ft_putchar(str[i]);
-        i++;
-    }
+	i = 0;
+	while (i < len)
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
 }
